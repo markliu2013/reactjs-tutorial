db468509552860b54b675527fd89f0d2e799c352
alexzhao201601 <markliu2013@gmail.com>
2020-05-12 16:51:14 +0800

    save

---
+++ src/render-props/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+import MouseTracker from './MouseTracker';
+
+ReactDOM.render(
+    <MouseTracker />,
+    document.getElementById('root')
+)
